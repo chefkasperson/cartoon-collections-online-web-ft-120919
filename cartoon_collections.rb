b7da0345c92f0c4c48e81864afe3c1f846f8a0e7
Erik Kasperson <chefkasperson@gmail.com>
2019-12-11 17:43:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,12 @@ def long_planeteer_calls(array)
   false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each {|item| 
+    if cheese_types.include?(item)
+      return item     
+    end
+  }
 end
