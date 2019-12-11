a16a23b727783279cdcb2fe30875be00e984c92e
Erik Kasperson <chefkasperson@gmail.com>
2019-12-11 17:29:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,12 +2,12 @@ def roll_call_dwarves(array)
   array.each_with_index(1) {|name, i| puts "#{i}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+  array.map {|element| element.capitalize}# Your code here
 end
 
-def long_planeteer_calls(array)
-  array.map {|element| element.capitalize}
+def long_planeteer_calls
+
 end
 
 def find_the_cheese# code an argument here
