54282e425864db0972407282a911bbbbe9a0af5a
Erik Kasperson <chefkasperson@gmail.com>
2019-12-11 17:13:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  array.each_with_index(1) {|name, i| puts "#{i}. #{name}"}
+  dwarves.each_with_index(1) {|name, i| puts "#{i}. #{name}"}
 
 end
 
