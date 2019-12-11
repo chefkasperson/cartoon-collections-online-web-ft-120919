9e832f513663121d22e7f2e123104677a829d3ec
Erik Kasperson <chefkasperson@gmail.com>
2019-12-11 17:39:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,13 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)
-  if array.length > 4
-    return true
-  else
-    return false
-  end
+  array.each {|word|
+    if word.join.length > 4
+      return true
+    else
+      return false
+    end
+  }
 end
 
 def find_the_cheese# code an argument here
